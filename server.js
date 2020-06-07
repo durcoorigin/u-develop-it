162f4a5fcf743272cbd5782b280c41ab9a9de037
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-06-07 13:00:24 -0500

    add voters

--- server.js
+++ server.js
@@ -2,23 +2,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express ();
-const sqlite3 = require('sqlite3').verbose();
 
 // Imported Modules
 const inputCheck = require('./utils/inputCheck');
+const db = require('./db/database');
 
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-const db = new sqlite3.Database('./db/election.db', err => {
-    if (err) {
-        return console.error(err.message);
-    }
-
-    console.log('Connected to the election database.');
-});
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
